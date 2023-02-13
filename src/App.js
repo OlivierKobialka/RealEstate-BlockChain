c0b4a3e89ec71ca239c783e68a21396e63fd2bad
OlivierKobialka <113932706+OlivierKobialka@users.noreply.github.com>
2023-02-13 17:12:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
+	const [account, setAccount] = useState(null);
 
-  return (
-    <div>
+	const loadBlockchaindata = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-      <div className='cards__section'>
+		const accounts = await window.ethereum.request({
+			method: "eth_requestAccounts",
+		});
+		setAccount(accounts[0]);
+	};
 
-        <h3>Welcome to Millow</h3>
+	useEffect(() => {
+		loadBlockchaindata();
+	}, []);
 
-      </div>
-
-    </div>
-  );
+	return (
+		<div>
+			<div className='cards__section'>
+				<h3>Welcome to Millow!</h3>
+			</div>
+		</div>
+	);
 }
 
 export default App;
