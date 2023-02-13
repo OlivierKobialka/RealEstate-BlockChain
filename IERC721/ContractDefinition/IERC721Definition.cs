a4c92ef7b56a91e10b899a67f7bdf2e1351eb6ba
OlivierKobialka <113932706+OlivierKobialka@users.noreply.github.com>
2023-02-13 13:52:45 +0100

    Add files via upload

---
+++ IERC721/ContractDefinition/IERC721Definition.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts;
+using System.Threading;
+
+namespace Millow.Contracts.IERC721.ContractDefinition
+{
+
+
+    public partial class IERC721Deployment : IERC721DeploymentBase
+    {
+        public IERC721Deployment() : base(BYTECODE) { }
+        public IERC721Deployment(string byteCode) : base(byteCode) { }
+    }
+
+    public class IERC721DeploymentBase : ContractDeploymentMessage
+    {
+        public static string BYTECODE = "";
+        public IERC721DeploymentBase() : base(BYTECODE) { }
+        public IERC721DeploymentBase(string byteCode) : base(byteCode) { }
+
+    }
+
+    public partial class TransferFromFunction : TransferFromFunctionBase { }
+
+    [Function("transferFrom")]
+    public class TransferFromFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "_from", 1)]
+        public virtual string From { get; set; }
+        [Parameter("address", "_to", 2)]
+        public virtual string To { get; set; }
+        [Parameter("uint256", "_id", 3)]
+        public virtual BigInteger Id { get; set; }
+    }
+
+
+}
---
+++ IERC721/IERC721Service.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts.ContractHandlers;
+using Nethereum.Contracts;
+using System.Threading;
+using Millow.Contracts.IERC721.ContractDefinition;
+
+namespace Millow.Contracts.IERC721
+{
+    public partial class IERC721Service
+    {
+        public static Task<TransactionReceipt> DeployContractAndWaitForReceiptAsync(Nethereum.Web3.Web3 web3, IERC721Deployment iERC721Deployment, CancellationTokenSource cancellationTokenSource = null)
+        {
+            return web3.Eth.GetContractDeploymentHandler<IERC721Deployment>().SendRequestAndWaitForReceiptAsync(iERC721Deployment, cancellationTokenSource);
+        }
+
+        public static Task<string> DeployContractAsync(Nethereum.Web3.Web3 web3, IERC721Deployment iERC721Deployment)
+        {
+            return web3.Eth.GetContractDeploymentHandler<IERC721Deployment>().SendRequestAsync(iERC721Deployment);
+        }
+
+        public static async Task<IERC721Service> DeployContractAndGetServiceAsync(Nethereum.Web3.Web3 web3, IERC721Deployment iERC721Deployment, CancellationTokenSource cancellationTokenSource = null)
+        {
+            var receipt = await DeployContractAndWaitForReceiptAsync(web3, iERC721Deployment, cancellationTokenSource);
+            return new IERC721Service(web3, receipt.ContractAddress);
+        }
+
+        protected Nethereum.Web3.Web3 Web3{ get; }
+
+        public ContractHandler ContractHandler { get; }
+
+        public IERC721Service(Nethereum.Web3.Web3 web3, string contractAddress)
+        {
+            Web3 = web3;
+            ContractHandler = web3.Eth.GetContractHandler(contractAddress);
+        }
+
+        public Task<string> TransferFromRequestAsync(TransferFromFunction transferFromFunction)
+        {
+             return ContractHandler.SendRequestAsync(transferFromFunction);
+        }
+
+        public Task<TransactionReceipt> TransferFromRequestAndWaitForReceiptAsync(TransferFromFunction transferFromFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(transferFromFunction, cancellationToken);
+        }
+
+        public Task<string> TransferFromRequestAsync(string from, string to, BigInteger id)
+        {
+            var transferFromFunction = new TransferFromFunction();
+                transferFromFunction.From = from;
+                transferFromFunction.To = to;
+                transferFromFunction.Id = id;
+            
+             return ContractHandler.SendRequestAsync(transferFromFunction);
+        }
+
+        public Task<TransactionReceipt> TransferFromRequestAndWaitForReceiptAsync(string from, string to, BigInteger id, CancellationTokenSource cancellationToken = null)
+        {
+            var transferFromFunction = new TransferFromFunction();
+                transferFromFunction.From = from;
+                transferFromFunction.To = to;
+                transferFromFunction.Id = id;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(transferFromFunction, cancellationToken);
+        }
+    }
+}
